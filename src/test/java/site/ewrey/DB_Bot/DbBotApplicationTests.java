7895d89a9e5d8b0ba661c3ec96bb0e3058618f8f
IooEWREYooI <yasha.manuilov@gmail.com>
2022-11-26 22:19:47 +0300

    Добавил Unit Testing

--- src/test/java/site/ewrey/DB_Bot/DbBotApplicationTests.java
+++ src/test/java/site/ewrey/DB_Bot/DbBotApplicationTests.java
@@ -1,13 +1,33 @@
 package site.ewrey.DB_Bot;
 
+import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static io.restassured.RestAssured.*;
 
-@SpringBootTest
+//@SpringBootTest
 class DbBotApplicationTests {
-
 	@Test
-	void contextLoads() {
+	void succesSendMessage() {
+		RestAssured.baseURI = "https://api.telegram.org/bot5782481564:AAEIqdv8PKRi3mR88EXbImRxn9QwJbPFaQk";
+		given()
+				.param("text", "rest-assured_TEST")
+				.param("chat_id", "362396673")
+				.when()
+				.get("/sendMessage")
+				.then()
+				.statusCode(200);
 	}
 
+	@Test
+	void unSuccessMessage(){
+		RestAssured.baseURI = "https://api.telegram.org/bot5782481564:AAEIqdv8PKRi3mR88EXbImRxn9QwJbPFaQk";
+		given()
+				.param("text", "rest-assured_TEST")
+				.param("chat_id", "362396673")
+				.param("parse_mode", "Markdown")
+				.when()
+				.get("/sendMessage")
+				.then()
+				.statusCode(400);
+	}
 }
